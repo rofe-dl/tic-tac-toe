c4b4bad94753e4a237072711676190e761ed9c81
rofe-dl <rafidul258@gmail.com>
2021-10-06 02:57:02 +0600

    refactored blocks to array

--- ai.js
+++ ai.js
@@ -1,2 +1,3 @@
-const {checkWin, blocks} = require('./index');
-
+const makeAiMove = () => {
+    
+}
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 let current = 'x';
 
+let statesTaken = [];
+let statesAvailable = [[0, 1, 2], [3, 4, 5], [6, 7, 8]
+                [0, 3, 6], [1, 4, 7], [2, 5, 8]
+                [0, 4, 8], [2, 4, 6]];
+
 const winnerDiv = document.querySelector('.winner');
 const winnerText = document.createElement('h3');
 
-const blocks = {
-    "0": document.getElementById('r1_c1'),
-    "1": document.getElementById('r1_c2'),
-    "2": document.getElementById('r1_c3'),
-    "3": document.getElementById('r2_c1'),
-    "4": document.getElementById('r2_c2'),
-    "5": document.getElementById('r2_c3'),
-    "6": document.getElementById('r3_c1'),
-    "7": document.getElementById('r3_c2'),
-    "8": document.getElementById('r3_c3'),
-}
+const blocks = [
+    document.getElementById('0'),
+    document.getElementById('1'),
+    document.getElementById('2'),
+    document.getElementById('3'),
+    document.getElementById('4'),
+    document.getElementById('5'),
+    document.getElementById('6'),
+    document.getElementById('7'),
+    document.getElementById('8'),
+]
 
 document.querySelector('#btn-clear').onclick = () => {
     winnerDiv.removeChild(winnerDiv.firstChild);
-    for (let b in blocks) {
-        blocks[b].classList.remove('marked');
-        blocks[b].classList.remove('x');
-        blocks[b].classList.remove('o');
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].classList.remove('marked');
+        blocks[i].classList.remove('x');
+        blocks[i].classList.remove('o');
     }
 }
 
@@ -29,38 +34,39 @@ document.querySelectorAll('.block').forEach((element) => {
         if (!element.classList.contains('marked')){
             element.classList.add('marked');
             element.classList.add(current);
+            
+
+            // statesTaken.push()
             current = current == 'x' ? 'o' : 'x';
             
+            makeAiMove();
             // checks if there's any winner after every move
             checkIfGameOver();
         }
     }
 })
 
-const isMarkedX = (index) => {
-    const element = blocks[index.toString()];
-    return element.classList.contains('x');
-}
-
-const isMarkedO = (index) => {
-    const element = blocks[index.toString()];
-    return element.classList.contains('o');
-}
-
-const isMarked = (index) => {
-    const element = blocks[index.toString()];
-    return element.classList.contains('marked');
-}
-
 const checkIfGameOver = () => {
     const winner = findWinner();
     if (winner === 'tie'){
-        winnerText.textContent = "The game is tied. Clear board to play again.";
+        winnerText.textContent = "The game is tied. Clear the board to play again.";
         winnerDiv.appendChild(winnerText);
     }else if (winner) {
         winnerText.textContent = winner.toUpperCase() + " wins! Clear board to play again.";
         winnerDiv.appendChild(winnerText);
     }
+}
+
+const isMarkedX = (index) => {
+    return blocks[index].classList.contains('x');
+}
+
+const isMarkedO = (index) => {
+    return blocks[index].classList.contains('o');
+}
+
+const isMarked = (index) => {
+    return blocks[index].classList.contains('marked');
 }
 
 const findWinner = () => {
