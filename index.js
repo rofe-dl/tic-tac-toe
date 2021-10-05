198161e2af8f935c8969dbc96f0a7ce2db1de54c
rofe-dl <rafidul258@gmail.com>
2021-10-05 21:47:58 +0600

    can find winner

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let current = 'x';
 
 const blocks = {
-    r1_c1: document.getElementById('r1_c1'),
-    r1_c2: document.getElementById('r1_c2'),
-    r1_c3: document.getElementById('r1_c3'),
-    r2_c1: document.getElementById('r2_c1'),
-    r2_c2: document.getElementById('r2_c2'),
-    r2_c3: document.getElementById('r2_c3'),
-    r3_c1: document.getElementById('r3_c1'),
-    r3_c2: document.getElementById('r3_c2'),
-    r3_c3: document.getElementById('r3_c3'),
+    "0": document.getElementById('r1_c1'),
+    "1": document.getElementById('r1_c2'),
+    "2": document.getElementById('r1_c3'),
+    "3": document.getElementById('r2_c1'),
+    "4": document.getElementById('r2_c2'),
+    "5": document.getElementById('r2_c3'),
+    "6": document.getElementById('r3_c1'),
+    "7": document.getElementById('r3_c2'),
+    "8": document.getElementById('r3_c3'),
 }
 
 const mark = (element) => {
@@ -17,6 +17,11 @@ const mark = (element) => {
         element.classList.add('marked');
         element.classList.add(current);
         current = current == 'x' ? 'o' : 'x';
+
+        const winner = checkWin();
+        if (checkWin()) {
+            console.log(winner + " wins!!");
+        }
     }
 }
 
@@ -34,3 +39,40 @@ document.querySelector('#btn-clear').onclick = () => {
     }
 }
 
+const isMarkedX = (index) => {
+    const element = blocks[index.toString()];
+    return element.classList.contains('x');
+}
+
+const isMarkedO = (index) => {
+    const element = blocks[index.toString()];
+    return element.classList.contains('o');
+}
+
+const checkWin = () => {
+    // horizontal
+    for (let i = 0; i <= 6; i = i + 3){
+        if (isMarkedX(i) && isMarkedX(i+1) && isMarkedX(i+2))
+            return 'x';
+        else if (isMarkedO(i) && isMarkedO(i+1) && isMarkedO(i+2))
+            return 'o';
+    }
+
+    // vertical
+    for (let i = 0; i <= 2; i++){
+        if (isMarkedX(i) && isMarkedX(i+3) && isMarkedX(i+6))
+            return 'x';
+        else if (isMarkedO(i) && isMarkedO(i+3) && isMarkedO(i+6))
+            return 'o';
+    }
+
+    // diagonal
+    for (let i = 0, j = 8; i <= 2; i = i + 2, j = j - 2){
+        if (isMarkedX(i) && isMarkedX(4) && isMarkedX(j))
+            return 'x';
+        else if (isMarkedO(i) && isMarkedO(4) && isMarkedO(j))
+            return 'o';
+    }
+
+    return false;
+}
